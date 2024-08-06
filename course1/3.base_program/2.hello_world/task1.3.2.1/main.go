16d1a08f40fb9f69820fd49038ccb49e5fe112ba
Oleg Samsonov <ghostkote@gmail.com>
2024-08-06 11:57:07 +0300

    hello_world 1.3.2.1

--- course1/3.base_program/2.hello_world/task1.3.2.1/main.go
+++ course1/3.base_program/2.hello_world/task1.3.2.1/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	HelloWorld()
+	fmt.Println(HelloWorld())
 }
 
-func HelloWorld() {
-	fmt.Println("Hello World!")
+func HelloWorld() string {
+	return "Hello World!"
 }
