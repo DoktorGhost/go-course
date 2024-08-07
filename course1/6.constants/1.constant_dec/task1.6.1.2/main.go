aa612731c3592003f3488bb3ae5ac02cbe53244f
Oleg Samsonov <ghostkote@gmail.com>
2024-08-07 11:24:43 +0300

    constant_dec 1.6.1.2

---
+++ course1/6.constants/1.constant_dec/task1.6.1.2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const (
+	monday    = 1
+	tuesday   = 2
+	wednesday = 3
+	thursday  = 4
+	friday    = 5
+	saturday  = 6
+	sunday    = 7
+)
+
+func main() {
+	fmt.Println(monday)
+	fmt.Println(tuesday)
+	fmt.Println(wednesday)
+	fmt.Println(thursday)
+	fmt.Println(friday)
+	fmt.Println(saturday)
+	fmt.Println(sunday)
+
+}
