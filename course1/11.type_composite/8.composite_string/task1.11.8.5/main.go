ecebbcc69051737647e018cdb88d3d6303509ee9
Oleg Samsonov <ghostkote@gmail.com>
2024-08-16 10:28:35 +0300

    composite string task 1.11.8.5 test

--- course1/11.type_composite/8.composite_string/task1.11.8.5/main.go
+++ course1/11.type_composite/8.composite_string/task1.11.8.5/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func ReverseString(str string) string {
 	strRune := []rune(str)
-	for i, j := 0, len(str)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, len(strRune)-1; i < j; i, j = i+1, j-1 {
 		strRune[i], strRune[j] = strRune[j], strRune[i]
 	}
 	return string(strRune)
@@ -13,5 +13,5 @@ func ReverseString(str string) string {
 func main() {
 	fmt.Println(ReverseString("Hello, World"))
 	fmt.Println(ReverseString("12345"))
-
+	fmt.Println(ReverseString("1"))
 }
