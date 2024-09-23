ec3d8b108c77400a1b491198dad1139900516cb9
Oleg Samsonov <ghostkote@gmail.com>
2024-09-23 11:28:46 +0300

    task 3.2.3.2 new

--- course3/2.server/3.server_http_router/task3.2.3.2/main.go
+++ course3/2.server/3.server_http_router/task3.2.3.2/main.go
@@ -10,14 +10,20 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/group1/{id}", getOneHandler)
-	r.Get("/group2/{id}", getTwoHandler)
-	r.Get("/group3/{id}", getThreeHandler)
+	r.Get("/group1/{id}", func(w http.ResponseWriter, r *http.Request) {
+		handleGroup(w, r, "Group 1")
+	})
+	r.Get("/group2/{id}", func(w http.ResponseWriter, r *http.Request) {
+		handleGroup(w, r, "Group 2")
+	})
+	r.Get("/group3/{id}", func(w http.ResponseWriter, r *http.Request) {
+		handleGroup(w, r, "Group 3")
+	})
 
 	http.ListenAndServe(":8080", r)
 }
 
-func getOneHandler(w http.ResponseWriter, r *http.Request) {
+func handleGroup(w http.ResponseWriter, r *http.Request, groupName string) {
 	id, err := strconv.Atoi(chi.URLParam(r, "id"))
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
@@ -25,38 +31,7 @@ func getOneHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	if id > 0 && id < 4 {
 		w.WriteHeader(http.StatusOK)
-		res := fmt.Sprintf("Group 1 Привет, мир %d", id)
-		w.Write([]byte(res))
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-	}
-
-}
-
-func getTwoHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	if id > 0 && id < 4 {
-		w.WriteHeader(http.StatusOK)
-		res := fmt.Sprintf("Group 2 Привет, мир %d", id)
-		w.Write([]byte(res))
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-	}
-}
-
-func getThreeHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	if id > 0 && id < 4 {
-		w.WriteHeader(http.StatusOK)
-		res := fmt.Sprintf("Group 3 Привет, мир %d", id)
+		res := fmt.Sprintf("%s Привет, мир %d", groupName, id)
 		w.Write([]byte(res))
 	} else {
 		w.WriteHeader(http.StatusNotFound)
